4af786e7a503758015bb57d28cf1464952ef91d3
Adam <pabek92@gmail.com>
2021-01-12 14:12:48 +0100

    dagoghshjghsljgsljgsadhgosghsbobjgbnjxnljknbsldjbnsldsnblsdnl

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {applyMiddleware, compose, createStore} from 'redux'
 import { roodReducer } from './Reduxs/roodReducer';
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
-import Menu from './pages/Menu';
+//import Menu from './pages/Menu';
 import Navbar from './Components/Navbar';
 
 function App() {
@@ -16,10 +16,12 @@ thunk
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 return (
-<Provider store={store}>
-  <Navbar />
-    <Menu />
-</Provider>
+<div className="cont">
+  <Provider store={store}>
+    <Navbar hoome="Home" abaut="Abaout" contact="Contacts" create="Create" 
+    myMenu="MyMenu" login="Login"/>
+  </Provider>
+</div>
 );
 }
 
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,19 +1,20 @@
-import React from 'react'
-
-const Navbar = ()=>{
+import { useState } from 'react'
+import '../StyleCss/navbar/onavbar.css'
+import icon from '../images/openMenu.png'
+const Navbar = (props)=>{
+const [isLoadin, setIsLoading] = useState(false)
 return(
-  <header>
-      <img className="logo" src="" alt="azam"/>
-      <nav>
-          <ul className="nav_links">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Login</a></li>
-          </ul>
-      </nav>
-      <a className="cta" href="#"><button>Contact</button></a>
-      
-  </header>
+<header className="header">
+    <img src={icon} alt={icon} className={isLoadin? 'icon' :''} onClick={()=>setIsLoading(!isLoadin)} />
+    <ul className={isLoadin? 'openNav' :'nav_links'} onClick={()=>setIsLoading(false)} >
+        {props.home ? <li><a href={props.h}>{props.home}</a></li>:''}
+        {props.abaut ? <li><a href={props.a}>{props.abaut}</a></li>:''}
+        {props.contact ? <li><a href={props.c}>{props.contact}</a></li>:''}
+        {props.create ? <li><a href={props.cr}>{props.create}</a></li>:''}
+        {props.myMenu ? <li><a href={props.my}>{props.myMenu}</a></li>:''}
+        {props.login ? <li><a href={props.l}>{props.login}</a></li>:''}
+    </ul>
+</header>
 )
 }
 export default Navbar
