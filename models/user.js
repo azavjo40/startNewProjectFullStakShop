41561666818760c0cc6f6c98d5def0b47183224b
tum.pl <63935755+azavjo40@users.noreply.github.com>
2021-01-13 20:06:08 +0100

    Update user.js
    Hhjdnndndnsndnn jdjdbne rjrhh

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema =  new Schema({
 name: {type: String, required: true},
 phone: {type: String, required: true},
 mail: {type: String, required: true},
-password: {type: String, required:true}
+password: {type: String, required:true},
+date: {type: Date, default: Date.now}
 })
 module.exports = model('User', userSchema)
